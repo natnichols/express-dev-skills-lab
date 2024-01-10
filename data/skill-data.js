999b8d2a6e23653fa83b044500fb46994fbb52da
NatNichols <natalie.e.nichols@gmail.com>
2024-01-09 22:43:23 -0500

    create views/skills directory and index.ejs file, add boilerplate and skills ejs to index.ejs, create data directory and data/skill-data.js

---
+++ data/skill-data.js
@@ -0,0 +1,9 @@
+const skills = [
+  {text: 'JavaScript', learned: true, _id: 202312},
+  {text: 'Node', learned: true, _id: 202401},
+  {text: 'React', learned: false, _id: 202402},
+]
+
+export {
+  skills
+}
