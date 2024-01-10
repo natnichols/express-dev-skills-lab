7a1db1bebfc5dbfa99f3eecdeca59fb5fd4eda1d
NatNichols <natalie.e.nichols@gmail.com>
2024-01-09 23:10:03 -0500

    create controllers directory and controller module for skills resource, move res.render function from routes/skills to controllers/skills and rename index, export index controller method, import skill-data to controllers/skills, import skillsCtrl to routes/skills, refactor route/skills get function to use skillsCtrl.index

--- routes/skills.js
+++ routes/skills.js
@@ -1,13 +1,9 @@
 import { Router } from 'express'
-import { skills } from '../data/skill-data.js'
+import * as skillsCtrl from '../controllers/skills.js'
 
 const router = Router()
 
 // GET localhost:3000/users
-router.get('/', function(req, res) {
-  res.render('skills/index', {
-    skills: skills
-  })
-})
+router.get('/', skillsCtrl.index)
 
 export { router }
